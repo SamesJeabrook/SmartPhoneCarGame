0f41512f5e3be3b335dc9af821fb67d8054a5a5a
MrSlips <james.seabrook@brandworkz.com>
2018-08-10 13:07:11 +0100

    Making the game ready for the web to test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
     http = require('http');
     app = express();
     server = http.createServer(app);
-    port = 8080;
+    port = process.env.PORT || 8080;
     game_sockets = {};
     controller_sockets = {};
 
